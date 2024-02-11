02586caf7a472fb7df4f55f0e7747eba6363c6f6
x0pK <mits87@gmail.com>
2024-02-11 16:40:14 +0100

    fix

--- src/components/google-analytics.tsx
+++ src/components/google-analytics.tsx
@@ -4,19 +4,18 @@ const GoogleAnalytics = ({ ga_id }: { ga_id: string }) => (
   <>
     <Script
       async
-      src={`https://www.googletagmanager.com/gtag/js? 
-      id=${ga_id}`}
+      src={`https://www.googletagmanager.com/gtag/js?id=${ga_id}`}
     ></Script>
     <Script
       id="google-analytics"
       dangerouslySetInnerHTML={{
         __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-          gtag('config', '${ga_id}');
-        `,
+        gtag('config', '${ga_id}');
+      `,
       }}
     ></Script>
   </>
